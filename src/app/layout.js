4b223cab82055a3e27f5b8cd7ad6c225b3df0175
slashMK303 <slashluck3@gmail.com>
2025-11-26 19:54:50 +0700

    ADD: Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -38,6 +39,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-ZKBCF9YN4X" />
     </html>
   );
 }
